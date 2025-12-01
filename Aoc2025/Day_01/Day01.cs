82087a68fd97536df0d82cc7abf6e6344172fa6c
Noud Savenije <noud.savenije@rabobank.nl>
2025-12-01 07:01:06 +0100

    nicer 1

--- Aoc2025/Day_01/Day01.cs
+++ Aoc2025/Day_01/Day01.cs
@@ -4,15 +4,10 @@ namespace Aoc2025.Day_01 {
     public static class Day01 {
         const string FILEPATH = "Day_01/input.txt";
         public static void Part1() {
-            var inputs = ParseLinesAsList(FILEPATH, x => {
-                return x;
-            });
             int pos = 50;
             int res = 0;
-            foreach (var rot in inputs)
+            foreach ((var dir, var dist) in ParseInput())
             {
-                char dir = rot[0];
-                int dist = int.Parse(rot[1..]);
                 pos = dir == 'L' ? pos - dist : pos + dist;
                 pos = ((pos % 100) + 100) % 100;
                 if (pos == 0)
@@ -21,26 +16,28 @@ namespace Aoc2025.Day_01 {
             Console.WriteLine(res);
         }
         public static void Part2() {
-            var inputs = ParseLinesAsList(FILEPATH, x => {
-                return x;
-            });
             int pos = 50;
             int res = 0;
-            foreach (var rot in inputs)
+            foreach ((var dir, var dist) in ParseInput())
             {
-                char dir = rot[0];
-                int dist = int.Parse(rot[1..]);
                 res += dist / 100;
-                dist %= 100;
+                int distMod = dist % 100;
 
-                pos = dir == 'L' ? pos - dist : pos + dist;
+                pos = dir == 'L' ? pos - distMod : pos + distMod;
 
-                if ((dir == 'L' && pos <= 0 && pos + dist != 0) || (dir == 'R' && pos >= 100))
+                if ((dir == 'L' && pos <= 0 && pos + distMod != 0) || (dir == 'R' && pos >= 100))
                     res++;
 
                 pos = ((pos % 100) + 100) % 100;
             }
             Console.WriteLine(res);
         }
+
+        private static List<(char,int)> ParseInput() =>
+            ParseLinesAsList(FILEPATH, x => {
+                char dir = x[0];
+                int dist = int.Parse(x[1..]);
+                return (dir,dist);
+            });
     }
 }
