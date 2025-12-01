2b73cd354c593726e9a7eea494463fa5afb464d9
Noud Savenije <noud.savenije@rabobank.nl>
2025-12-01 06:32:28 +0100

    This is gonna take some getting used to again. Day 1

--- Aoc2025/Day_01/Day01.cs
+++ Aoc2025/Day_01/Day01.cs
@@ -4,15 +4,64 @@ namespace Aoc2025.Day_01 {
     public static class Day01 {
         const string FILEPATH = "Day_01/input.txt";
         public static void Part1() {
-            // TODO: Implement Part 1
             var inputs = ParseLinesAsList(FILEPATH, x => {
                 return x;
             });
-            var matrix = ParseMatrix(FILEPATH, x => x - '0');
+            int pos = 50;
+            int res = 0;
+            foreach (var rot in inputs)
+            {
+                char dir = rot[0];
+                int dist = int.Parse(rot[1..]);
+                if (dir == 'L')
+                {
+                    pos = (pos - dist) % 100;
+                }
+                else
+                {
+                    pos = (pos + dist) % 100;
+                }
+                if (pos == 0)
+                {
+                    res++;
+                }
+            }
+            Console.WriteLine(res);
         }
         public static void Part2() {
-            // TODO: Implement Part 2
-            Console.WriteLine("Day 01, Part 2: Not implemented yet.");
+            var inputs = ParseLinesAsList(FILEPATH, x => {
+                return x;
+            });
+            int pos = 50;
+            int res = 0;
+            foreach (var rot in inputs)
+            {
+                char dir = rot[0];
+                int dist = int.Parse(rot[1..]);
+                res += dist / 100;
+                dist %= 100;                
+
+                if (dir == 'L')
+                {
+                    pos -= dist;
+                    if (pos <= 0 && (pos + dist != 0))
+                    {
+                        res++;
+                        
+                    }
+                    pos = ((pos % 100) + 100) % 100;
+                }
+                else
+                {
+                    pos += dist;
+                    if (pos >= 100)
+                    {
+                        res++;
+                    }
+                    pos %= 100;   
+                }
+            }
+            Console.WriteLine(res);
         }
     }
 }
