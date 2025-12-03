16f8006b950ea1f656db1ea0711d686b55d7c5ab
Noud Savenije <noud.savenije@rabobank.nl>
2025-12-03 09:52:46 +0100

    even worse!

--- Aoc2025/Day_03/Day03.cs
+++ Aoc2025/Day_03/Day03.cs
@@ -6,17 +6,14 @@ namespace Aoc2025.Day_03 {
         const string FILEPATH = "Day_03/input.txt";
         
         public static void Part1() =>
-            Console.WriteLine(ParseLinesAsList(FILEPATH, x => x.Select(c => c - '0').ToArray()).Sum(bank => SolveBank(bank, 2)));
+            Console.WriteLine(ParseLinesAsList(FILEPATH, x => x.Select(c => c - '0').ToArray()).Sum(bank => SolveBank(bank, 1)));
         
         public static void Part2() =>
-            Console.WriteLine(ParseLinesAsList(FILEPATH, x => x.Select(c => c - '0').ToArray()).Sum(bank => SolveBank(bank, 12)));
+            Console.WriteLine(ParseLinesAsList(FILEPATH, x => x.Select(c => c - '0').ToArray()).Sum(bank => SolveBank(bank, 11)));
 
-        private static long SolveBank(int[] bank, int requiredBatteries) =>
-             Enumerable.Range(0, requiredBatteries)
-                .Aggregate((index: 0, res: 0L), (state, counter) => 
-                    UpdateIndexAndRes(bank, state.index, state.res, requiredBatteries - 1, counter)).res;
-
-        private static (int, long) UpdateIndexAndRes(int[] bank, int index, long res, int bat, int counter)
-            => (index+Array.IndexOf(bank[index..(bank.Length-(bat-counter))],bank[index..(bank.Length-(bat-counter))].Max())+1,res+bank[index..(bank.Length-(bat-counter))].Max()*(long)Math.Pow(10,bat-counter));
+        private static long SolveBank(int[] bank, int l) =>
+             Enumerable.Range(0, l + 1)
+                .Aggregate((i: 0, res: 0L), (s, c) => 
+                    (s.i+Array.IndexOf(bank[s.i..(bank.Length-(l-c))],bank[s.i..(bank.Length-(l-c))].Max())+1,s.res+bank[s.i..(bank.Length-(l-c))].Max()*(long)Math.Pow(10,l-c))).res;
     }
 }
