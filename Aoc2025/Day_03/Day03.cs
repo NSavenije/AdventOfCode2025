cb36e4a1bf5c7da79ec0a2afa9d27c7e381c0078
Noud Savenije <noud.savenije@rabobank.nl>
2025-12-03 10:08:10 +0100

    latest one liner

--- Aoc2025/Day_03/Day03.cs
+++ Aoc2025/Day_03/Day03.cs
@@ -2,18 +2,10 @@ namespace Aoc2025.Day_03 {
     using System.Linq;
     using static InputParser;
     public static class Day03 {
-
-        const string FILEPATH = "Day_03/input.txt";
-        
         public static void Part1() =>
-            Console.WriteLine(ParseLinesAsList(FILEPATH, x => x.Select(c => c - '0').ToArray()).Sum(bank => SolveBank(bank, 1)));
+            Console.WriteLine(ParseLinesAsList("Day_03/input.txt", x => x.Select(c => c - '0').ToArray()).Sum(b => Enumerable.Range(0, 2).Aggregate((i: 0, res: 0L), (s, c) => (s.i+Array.IndexOf(b[s.i..(b.Length-(1-c))],b[s.i..(b.Length-(1-c))].Max())+1,s.res+b[s.i..(b.Length-(1-c))].Max()*(long)Math.Pow(10,1-c))).res));
         
         public static void Part2() =>
-            Console.WriteLine(ParseLinesAsList(FILEPATH, x => x.Select(c => c - '0').ToArray()).Sum(bank => SolveBank(bank, 11)));
-
-        private static long SolveBank(int[] bank, int l) =>
-             Enumerable.Range(0, l + 1)
-                .Aggregate((i: 0, res: 0L), (s, c) => 
-                    (s.i+Array.IndexOf(bank[s.i..(bank.Length-(l-c))],bank[s.i..(bank.Length-(l-c))].Max())+1,s.res+bank[s.i..(bank.Length-(l-c))].Max()*(long)Math.Pow(10,l-c))).res;
+            Console.WriteLine(ParseLinesAsList("Day_03/input.txt", x => x.Select(c => c - '0').ToArray()).Sum(b => Enumerable.Range(0, 12).Aggregate((i: 0, res: 0L), (s, c) => (s.i+Array.IndexOf(b[s.i..(b.Length-(11-c))],b[s.i..(b.Length-(11-c))].Max())+1,s.res+b[s.i..(b.Length-(11-c))].Max()*(long)Math.Pow(10,11-c))).res));
     }
 }
