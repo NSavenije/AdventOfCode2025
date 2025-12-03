04c7b6e4edb97d13976ac205f2e09e7e7808bf7f
Noud Savenije <noud.savenije@rabobank.nl>
2025-12-03 06:45:30 +0100

    logic from 2 to 1

--- Aoc2025/Day_03/Day03.cs
+++ Aoc2025/Day_03/Day03.cs
@@ -6,35 +6,11 @@ namespace Aoc2025.Day_03 {
         const string FILEPATH = "Day_03/input.txt";
         
         public static void Part1() {
-            // TODO: Implement Part 1
             var banks = ParseLinesAsList(FILEPATH, x => x.Select(c => c - '0').ToArray());
             long sum = 0;
             foreach(var bank in banks)
             {
-                int largest = bank[0];
-                int second = 0;
-                int index = 0;
-                int secondIndex = 0;
-                
-                for (int i = 1; i < bank.Length - 1; i++)
-                {
-                    if (bank[i] > largest)
-                    {
-                        largest = bank[i];
-                        index = i;
-                    }
-                }
-                for (int i = index + 1; i < bank.Length; i++)
-                {
-                    if (bank[i] >= second)
-                    {
-                        second = bank[i];
-                        secondIndex = i;
-                    }
-                }
-                int res = 10 * largest + second;
-                Console.WriteLine(res);
-                sum += res;
+                sum += SolveBank(bank, 2);
             }
             Console.WriteLine(sum);
         }
@@ -43,27 +19,29 @@ namespace Aoc2025.Day_03 {
             long sum = 0;
             foreach(var bank in banks)
             {
-                int largest;
-                int index = 0;
-                long res = 0;
-                
-                for (int counter = 0; counter < 12; counter++)
-                {
-                    largest = 0;
-                    for (int i = index; i < bank.Length - (11 - counter); i++)
-                    {
-                        if (bank[i] > largest)
-                        {
-                            largest = bank[i];
-                            index = i + 1;
-                        }
-                    }
-                    res += largest * (long)Math.Pow(10, 11 - counter);
-                }
-                Console.WriteLine(res);
-                sum += res;
+                sum += SolveBank(bank, 12);
             }
             Console.WriteLine(sum);
+        }
+        private static long SolveBank(int[] bank, int requiredBatteries)
+        {
+            int index = 0;
+            long res = 0;
+            int bat = requiredBatteries - 1;
+            for (int counter = 0; counter <= bat; counter++)
+            {
+                int largest = 0;
+                for (int i = index; i < bank.Length - (bat - counter); i++)
+                {
+                    if (bank[i] > largest)
+                    {
+                        largest = bank[i];
+                        index = i + 1;
+                    }
+                }
+                res += largest * (long)Math.Pow(10, bat - counter);
+            }
+            return res;
         }
     }
 }
