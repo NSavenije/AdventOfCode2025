7598bb06c9f8938bf5a968be1d30d9290126976e
Noud Savenije <noud.savenije@rabobank.nl>
2025-12-05 08:10:41 +0100

    elegant maar fout

--- Aoc2025/Day_05/Day05.cs
+++ Aoc2025/Day_05/Day05.cs
@@ -43,8 +43,34 @@ namespace Aoc2025.Day_05 {
             Console.WriteLine(freshCounter);
         }
         public static void Part2() {
-            // TODO: Implement Part 2
-            Console.WriteLine("Day 05, Part 2: Not implemented yet.");
+            var input = ParseLinesAsList(FILEPATH, c => c);
+            List<(long st,long en)> ranges = [];
+
+            bool parsingRanges = true;
+
+            foreach (string line in input)
+            {
+                if (line == "")
+                {
+                    break;
+                } 
+                if (parsingRanges)
+                {
+                    string[] items = line.Split('-');
+                    long start = long.Parse(items[0]);
+                    long end = long.Parse(items[1]);
+                    ranges.Add((start, end));
+                }
+            }
+            ranges.Sort();
+            long count = 0;
+            long pos = 0;
+            foreach((long start, long end) in ranges)
+            {
+                count += Math.Max(0, end - Math.Max(start - 1, pos));
+                pos = end;
+            }
+            Console.WriteLine(count);
         }
     }
 }
