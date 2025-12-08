c9d3f0be13de5dae0b2bbc77e367088bbf6f39b8
Noud Savenije <noud.savenije@rabobank.nl>
2025-12-08 11:29:37 +0100

    finally 8

--- Aoc2025/Day_08/Day08.cs
+++ Aoc2025/Day_08/Day08.cs
@@ -1,13 +1,91 @@
 namespace Aoc2025.Day_08 {
+    using static InputParser;
     public static class Day08 {
         const string FILEPATH = "Day_08/input.txt";
         public static void Part1() {
-            // TODO: Implement Part 1
-            Console.WriteLine("Day 08, Part 1: Not implemented yet.");
+            List<(int,int,int)> lines = ParseLinesAsList(FILEPATH, c =>
+            {
+                int[] nums = c.Split(',').Select(int.Parse).ToArray();
+                return (nums[0], nums[1], nums[2]);
+            });
+            SortedDictionary<double, ((int,int,int) p ,(int,int,int) q)> sortedPairs = [];
+            List<HashSet<(int,int,int)>> circuits = [];
+            foreach(var p in lines)
+            {
+                circuits.Add([p]);
+
+                foreach(var q in lines)
+                {
+                    if (p == q)
+                        continue;
+                    sortedPairs.TryAdd(EuclidianDistance(p,q),(p,q));
+                }
+            }
+            for (int i = 0; i < 1000; i++)
+            {
+                var(p,q) = sortedPairs.ElementAt(i).Value;
+                var setP = circuits.FirstOrDefault(s => s.Contains(p));
+                var setQ = circuits.FirstOrDefault(s => s.Contains(q));
+                if (setP != null && setQ != null && setP != setQ)
+                {
+                    setP.UnionWith(setQ);
+                    circuits.Remove(setQ);
+                }
+            }
+            circuits.Sort((a, b) => b.Count.CompareTo(a.Count));
+            
+            long res = circuits[0].Count * circuits[1].Count * circuits[2].Count;
+
+            Console.WriteLine(res);
         }
+
         public static void Part2() {
-            // TODO: Implement Part 2
-            Console.WriteLine("Day 08, Part 2: Not implemented yet.");
+            List<(int,int,int)> lines = ParseLinesAsList(FILEPATH, c =>
+            {
+                int[] nums = c.Split(',').Select(int.Parse).ToArray();
+                return (nums[0], nums[1], nums[2]);
+            });
+            SortedDictionary<double, ((int,int,int) p ,(int,int,int) q)> sortedPairs = [];
+            List<HashSet<(int,int,int)>> circuits = [];
+            foreach(var p in lines)
+            {
+                circuits.Add([p]);
+
+                foreach(var q in lines)
+                {
+                    if (p == q)
+                        continue;
+                    sortedPairs.TryAdd(EuclidianDistance(p,q),(p,q));
+                }
+            }
+            long res = -1;
+            for (int i = 0; i < 1000000; i++)
+            {
+                var(p,q) = sortedPairs.ElementAt(i).Value;
+                var setP = circuits.FirstOrDefault(s => s.Contains(p));
+                var setQ = circuits.FirstOrDefault(s => s.Contains(q));
+                if (setP != null && setQ != null && setP != setQ)
+                {
+                    if (circuits.Count == 2)
+                    {
+                        res = (long)p.Item1 * q.Item1;
+                        break;
+                    }
+                    setP.UnionWith(setQ);
+                    circuits.Remove(setQ);
+                }
+            }
+        
+
+            Console.WriteLine(res);
+        }
+
+        private static double EuclidianDistance((int x, int y, int z) p, (int x, int y, int z) q)
+        {
+            double x = Math.Pow(p.x - q.x, 2);
+            double y = Math.Pow(p.y - q.y, 2);
+            double z = Math.Pow(p.z - q.z, 2);
+            return Math.Sqrt(x+y+z);
         }
     }
 }
