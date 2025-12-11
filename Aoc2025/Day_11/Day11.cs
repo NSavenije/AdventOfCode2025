e55b5b5732d1420f4a042b3f458b1f62c997c285
Noud Savenije <noud.savenije@rabobank.nl>
2025-12-11 06:27:21 +0100

    too slow

--- Aoc2025/Day_11/Day11.cs
+++ Aoc2025/Day_11/Day11.cs
@@ -31,8 +31,36 @@ namespace Aoc2025.Day_11 {
             Console.WriteLine(output);
         }
         public static void Part2() {
-            // TODO: Implement Part 2
-            Console.WriteLine("Day 11, Part 2: Not implemented yet.");
+            var lines = ParseLinesAsList(FILEPATH);
+            Dictionary<string,List<string>> servers = [];
+            foreach(var line in lines)
+            {
+                var paths = line.Split(':', StringSplitOptions.TrimEntries);
+                servers.Add(paths[0], paths[1].Split(' ').ToList());
+            }
+            Queue<(string me, string state)> queue = [];
+            queue.Enqueue(("svr", ""));
+            var visited = new HashSet<(string, string)>();
+            long output = 0;
+            while(queue.Count > 0)
+            {
+                var (s, state) = queue.Dequeue();
+                if (!visited.Add((s, state)))
+                    continue;
+                foreach(var server in servers[s])
+                {
+                    if (server == "out")
+                    {
+                        if (state.Contains("fft") && state.Contains("dac"))
+                            output++;
+                    }
+                    else
+                    {
+                        queue.Enqueue((server, $"{state},{s}"));
+                    }
+                }
+            }
+            Console.WriteLine(output);
         }
     }
 }
