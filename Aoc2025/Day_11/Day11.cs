c65ec0bffbc0cfa88916734a4972e1c1aedeee4f
Noud Savenije <noud.savenije@rabobank.nl>
2025-12-11 06:10:40 +0100

    11 - 1

--- Aoc2025/Day_11/Day11.cs
+++ Aoc2025/Day_11/Day11.cs
@@ -1,9 +1,34 @@
 namespace Aoc2025.Day_11 {
+    using static InputParser;
     public static class Day11 {
         const string FILEPATH = "Day_11/input.txt";
         public static void Part1() {
-            // TODO: Implement Part 1
-            Console.WriteLine("Day 11, Part 1: Not implemented yet.");
+            var lines = ParseLinesAsList(FILEPATH);
+            Dictionary<string,List<string>> servers = [];
+            foreach(var line in lines)
+            {
+                var paths = line.Split(':', StringSplitOptions.TrimEntries);
+                servers.Add(paths[0], paths[1].Split(' ').ToList());
+            }
+            Queue<string> queue = [];
+            queue.Enqueue("you");
+            long output = 0;
+            while(queue.Count > 0)
+            {
+                string s = queue.Dequeue();
+                foreach(var server in servers[s])
+                {
+                    if (server == "out")
+                    {
+                        output++;
+                    }
+                    else
+                    {
+                        queue.Enqueue(server);
+                    }
+                }
+            }
+            Console.WriteLine(output);
         }
         public static void Part2() {
             // TODO: Implement Part 2
